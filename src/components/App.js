7c4c397d4e8adcd0a0bcb122bfb24feb91a2a7d2
Kapil Chaudhary <kc685838@gmail.com>
2023-05-11 16:01:22 +0530

    submitting code-Thu May 11 2023 16:01:19 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Tabs from './Tabs';
+import Viewport from './Viewport';
 
+const App = () => {
+    return (
+        <div>
+            <Tabs />
+            <Viewport />
+        </div>
+    );
+}
+
+export default App;
